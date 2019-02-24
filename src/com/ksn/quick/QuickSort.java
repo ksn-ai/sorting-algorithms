2205c266ea46019f789faa111050965e89a58ada
Kishan Singh <ksn1.cs@gmail.com>
2019-02-23 22:14:19 -0500

    Update QuickSort.java

--- src/com/ksn/quick/QuickSort.java
+++ src/com/ksn/quick/QuickSort.java
@@ -14,28 +14,30 @@ public class QuickSort {
     int partition(int arr[], int low, int high)
     {
         int pivot = arr[high];
-        int i = (low-1); // index of smaller element
+        int pIndex = (low); // index of smaller element
         for (int j=low; j<high; j++)
         {
             // If current element is smaller than or
             // equal to pivot
             if (arr[j] <= pivot)
             {
-                i++;
+
 
                 // swap arr[i] and arr[j]
-                int temp = arr[i];
-                arr[i] = arr[j];
+                int temp = arr[pIndex];
+                arr[pIndex] = arr[j];
                 arr[j] = temp;
+
+                pIndex++;
             }
         }
 
-        // swap arr[i+1] and arr[high] (or pivot)
-        int temp = arr[i+1];
-        arr[i+1] = arr[high];
+        // swap arr[pIndex] and arr[high] (or pivot)
+        int temp = arr[pIndex];
+        arr[pIndex] = arr[high];
         arr[high] = temp;
 
-        return i+1;
+        return pIndex;
     }
 
 
@@ -49,19 +51,19 @@ public class QuickSort {
         {
             /* pi is partitioning index, arr[pi] is
               now at right place */
-            int pi = partition(arr, low, high);
+            int pIndex = partition(arr, low, high);
 
             // Recursively sort elements before
             // partition and after partition
-            sort(arr, low, pi-1);
-            sort(arr, pi+1, high);
+            sort(arr, low, pIndex-1);
+            sort(arr, pIndex+1, high);
         }
     }
 
 
     public static void main(String args[])
     {
-        int arr[] = {10, 7, 8, 9, 1, 5};
+        int arr[] = {10, 7, 8, 9, 1, 5,34,12,4,7,41};
         int n = arr.length;
 
         QuickSort ob = new QuickSort();
